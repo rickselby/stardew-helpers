f1382d3b12d46de2675bc5f9f98584a1178a50ce
Rick Selby <rick@selby-family.co.uk>
2018-02-28 15:09:57 +0000

    Add map links to the schedules

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -18,3 +18,12 @@ $.ajaxSetup({
         'X-CSRF-TOKEN': $('meta[name=csrf-token]').attr("content"),
     }
 });
+
+$(document).ready(function() {
+    $('#mapModal').on('show.bs.modal', function (event) {
+        // Using .data() seems to cache the information, so when the vue app changes the links, we get the old images
+        // Using .attr() doesn't cache anything...?
+        var map = $(event.relatedTarget).attr('data-map');
+        $(this).find('IMG').attr('src', 'map/' + map);
+    });
+});
