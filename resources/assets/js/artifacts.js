1f48a8b2b9074e38970c489023225210f54100dc
Rick Selby <rick@selby-family.co.uk>
2018-03-01 12:09:09 +0000

    Set artifact map names and get the correct farm map

--- resources/assets/js/artifacts.js
+++ resources/assets/js/artifacts.js
@@ -19,11 +19,10 @@ $(document).ready(function() {
 
         reader.onload = function (e) {
             var output = "",
-                xmlDoc = $.parseXML(e.target.result);
+                xmlDoc = $.parseXML(e.target.result),
+                farmType = $(xmlDoc).find('whichFarm').text();
 
-            output += showLocations(getArtifactSpots(xmlDoc));
-
-            console.log(output);
+            output += showLocations(getArtifactSpots(xmlDoc), farmType);
 
             $('#out').html(output);
 
@@ -55,14 +54,50 @@ $(document).ready(function() {
         return locations;
     }
 
-    function showLocations(locations) {
+    function showLocations(locations, farmType) {
         var output = "";
         $.each(locations, function(id, location) {
-            output += '<h3>' + location.map + '</h3>';
+            output += '<h3>' + getMapName(location.map) + '</h3>';
             $.each(location.spots, function(id, spot) {
-                output += '<span class="artifactMap"><img src="map/' + location.map + '/' + spot.x + '/' + spot.y + '" /></span>';
+                output += '<span class="artifactMap"><img src="map/' + getMapReference(location.map, farmType) + '/' + spot.x + '/' + spot.y + '" /></span>';
             });
         });
         return output;
     }
+
+    function getMapName(map) {
+        var artifactMaps = {
+            BusStop: 'Bus Stop',
+            Woods: 'Hidden Forest'
+        }
+
+        if (artifactMaps.hasOwnProperty(map)) {
+            return artifactMaps[map];
+        } else {
+            return map;
+        }
+    }
+
+    function getMapReference(map, farmType) {
+        if (map === 'Farm') {
+            console.log(map, farmType);
+            switch(farmType) {
+                case '0':
+                    return 'Farm';
+                case '1':
+                    return 'FarmFishing';
+                case '2':
+                    return 'FarmForage';
+                case '3':
+                    return 'FarmMining';
+                case '4':
+                    return 'FarmCombat';
+                default:
+                    return 'WHAT';
+            }
+        } else {
+            return map;
+        }
+    }
+
 });
