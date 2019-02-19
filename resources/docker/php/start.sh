0b6b4de9f05d994821b995c365f6a7d0a1a50018
Rick Selby <rick@selby-family.co.uk>
2019-02-19 13:23:22 +0000

    Buildkit

--- resources/docker/php/start.sh
+++ resources/docker/php/start.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-role=${CONTAINER_ROLE:-app}
 env=${APP_ENV:-production}
 
 if [ "$env" != "local" ]; then
@@ -10,28 +9,5 @@ if [ "$env" != "local" ]; then
     (cd /code && php artisan config:cache && php artisan route:cache)
 fi
 
-if [ "$role" = "app" ]; then
-
-    echo "Migrating and updating permissions..."
-    (cd /code && php artisan migrate --force && php artisan permission:update)
-    echo "Running the app..."
-    exec php-fpm
-
-elif [ "$role" = "queue" ]; then
-
-    echo "Running the queue..."
-    php /code/artisan queue:work --verbose --tries=3 --timeout=90
-
-elif [ "$role" = "scheduler" ]; then
-
-    echo "Running the scheduler..."
-    while [ true ]
-    do
-      php /code/artisan schedule:run --verbose --no-interaction &
-      sleep 60
-    done
-
-else
-    echo "Could not match the container role \"$role\""
-    exit 1
-fi
+echo "Running the app..."
+exec php-fpm
