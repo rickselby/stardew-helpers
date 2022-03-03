4afd9ec498f7edacbb01ead1a590a9f5d5299c3c
Rick Selby <rick@selby-family.co.uk>
2022-03-03 20:49:19 +0000

    Set up webpack, vue, bootstrap 5

---
+++ vue/forage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="row panel">
+    <div class="wood-border">
+      <h1>Forage</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "forage"
+}
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const AssetsPlugin = require('assets-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: [
+            './vue/app.js',
+            './sass/app.scss'
+        ]
+    },
+    output: {
+        filename: 'js/[name].js',
+        path: path.resolve(__dirname, 'public'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/i,
+                loader: 'vue-loader',
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.png$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'img/[name][ext][query]'
+                }
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext][query]'
+                }
+            },
+        ],
+    },
+    plugins: [
+        new AssetsPlugin({
+            path: path.resolve(__dirname, 'public'),
+            filename: 'manifest.json'
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "css/[name].css",
+            chunkFilename: "[id].css",
+        }),
+        new VueLoaderPlugin(),
+    ],
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+        }
+    }
+};
