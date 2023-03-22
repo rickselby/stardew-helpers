0fca9733416cf26a4fea3afbd63785c39a3d4ba8
Rick Selby <rick@selby-family.co.uk>
2023-03-22 20:48:00 +0000

    Set calendar marker position

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,11 +1,22 @@
 module CalendarHelper
   def get_coords(day)
-    row, day_of_week = (day - 1).divmod 7
+    row, day_of_week = coords_for day
     [
-      (day_of_week * 32) + 9,
-      (row * 32) + 39,
-      (day_of_week * 32) + 41,
-      (row * 32) + 71,
+      day_of_week + 9,
+      row + 39,
+      day_of_week + 41,
+      row + 71,
     ].join ','
+  end
+
+  def calendar_marker_position(day)
+    row, day_of_week = coords_for day
+    left = 7 + day_of_week
+    top = 37 + row
+    "left: #{left}px; top: #{top}px;"
+  end
+
+  def coords_for(day)
+    (day.to_i - 1).divmod(7).map { |e| e * 32 }
   end
 end
