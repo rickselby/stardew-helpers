89c539398bcf0525ae9a4b84e47b5d66950ada81
Rick Selby <rick@selby-family.co.uk>
2023-10-16 17:39:53 +0100

    Switch away from imagemap to use turbo frames properly

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,19 +1,19 @@
 module CalendarHelper
-  def get_coords(day)
-    row, day_of_week = coords_for day
-    [
-      day_of_week + 9,
-      row + 39,
-      day_of_week + 41,
-      row + 71,
-    ].join ','
-  end
-
   def calendar_marker_position(day)
     row, day_of_week = coords_for day
-    left = 7 + day_of_week
-    top = 37 + row
-    "left: #{left}px; top: #{top}px;"
+    "left: #{day_of_week + 7}px; top: #{row + 37}px;"
+  end
+
+  def day_link_position(day)
+    row, day_of_week = coords_for day
+    [
+      "position: absolute",
+      "left: #{day_of_week + 9}px",
+      "top: #{row + 39}px",
+      "width: 32px",
+      "height: 32px",
+      "background-color: transparent"
+    ].join ";"
   end
 
   def coords_for(day)
