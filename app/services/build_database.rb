9c4b8458c482ea58bc409799276439f4fe131196
Rick Selby <rick@selby-family.co.uk>
2023-11-30 07:51:42 +0000

    Load schedules and names into the database

--- app/services/build_database.rb
+++ app/services/build_database.rb
@@ -2,20 +2,25 @@ class BuildDatabase
   def load_people
     Person.destroy_all
     Stardew::People.each do |person_name|
-      Person.create name: person_name
-      load_schedules person_name
+      person = Person.create name: person_name
+      load_schedules person
     end
   end
 
-  def load_schedules(person_name)
+  def load_schedules(person)
     Stardew::SEASONS.each do |season|
       Stardew::DAYS.each do |day|
-        load_schedule person_name, season, day
+        load_schedule person, season, day
       end
     end
   end
 
-  def load_schedule(person_name, season, day)
-    possibilities = Stardew::Schedules.new(person_name).schedule(season, day)
+  def load_schedule(person, season, day)
+    Stardew::Schedules.new(person.name).schedule(season, day).each do |possibility|
+      # TODO later: deduplicate the schedules
+      schedule = Schedule.create(reference: possibility.notes)
+      # TODO: load the locations into the schedule
+      person.person_schedules.create(schedule:, season:, day:, order: possibility.priority)
+    end
   end
 end
